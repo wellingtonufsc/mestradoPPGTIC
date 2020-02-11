8d37f21deac966671c0a1cb2ba7aaf883fe602fc
Hamilton DIas <50336625+Hamilton-Dias@users.noreply.github.com>
2020-02-11 17:48:38 -0300

    Add files via upload

---
+++ client-iota/build/precache-manifest.df1f4cf15f123fbc5c0cc550f59dfeac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a166c6fd9b4cbb857237bef7cf49b950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ddce68f5cc20804c0a0",
+    "url": "/static/css/main.5506eddc.chunk.css"
+  },
+  {
+    "revision": "73b6d8feb53daf31a23c",
+    "url": "/static/js/2.220655fe.chunk.js"
+  },
+  {
+    "revision": "88a1d54e4fbde9aa3544e0379b90d125",
+    "url": "/static/js/2.220655fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ddce68f5cc20804c0a0",
+    "url": "/static/js/main.965a2a98.chunk.js"
+  },
+  {
+    "revision": "0d11717b9b04d89adcfe",
+    "url": "/static/js/runtime-main.588085b2.js"
+  }
+]);
