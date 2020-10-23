191f0770f60e3b29926645ebd88d649ba482a501
Hamilton <hamiltondias1997@hotmail.com>
2020-10-23 12:31:27 -0300

    fixed dashboard pages

--- appiotafront/src/products/pages/Dashboard.js
+++ appiotafront/src/products/pages/Dashboard.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import NavbarCustom from '../../shared/components/navigation/NavbarCustom';
 import Sidebar from '../../shared/components/navigation/Sidebar';
+import api from '../../services/api';
+import { AuthContext } from '../../shared/context/auth-context';
+import { ToastContainer, toast } from 'react-toastify';
 import './Dashboard.scss';
+import AddProduct from '../components/addProduct';
 
 const Dashboard = () => {
+
+    const [products, setProducts] = useState({});
+    const auth = useContext(AuthContext);
+
+    useEffect(() => {
+        async function getProducts() {
+            api.get('/products/' + auth.userId)
+            .then((response) => {
+                setProducts(response.data.products);
+                console.log(response);
+            })
+            .catch((error) => {
+                toast(error.response.data.message, {type:'error'})
+            });
+        }
+        getProducts();
+    }, [auth.userId]);
 
     return(
         <div className="dashboard">
             <NavbarCustom></NavbarCustom>
-            <Sidebar ></Sidebar>
+            <Sidebar products={products}></Sidebar>
+            <ToastContainer 
+                position="top-left"
+            />
+
+            <div className="dashboard-content">
+                <AddProduct></AddProduct>
+            </div>
         </div>
     );
 }
--- appiotafront/src/shared/components/navigation/Sidebar.js
+++ appiotafront/src/shared/components/navigation/Sidebar.js
@@ -1,38 +1,22 @@
-import React, { useEffect, useContext } from 'react';
+import React from 'react';
 import { FaProductHunt } from 'react-icons/fa';
-import api from '../../../services/api';
-import { AuthContext } from '../../context/auth-context';
 import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-pro-sidebar/dist/css/styles.css';
 import './Sidebar.scss';
 
-const Sidebar = () => {
-
-    const auth = useContext(AuthContext);
-
-    useEffect(() => {
-        async function getProducts() {
-            api.get('/products/' + auth.userId)
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                toast(error.response.data.message, {type:'error'})
-            });
-        }
-        getProducts();
-    }, [auth.userId]);
+const Sidebar = props => {
 
     return (
         <ProSidebar collapsed >
-            <ToastContainer 
-                position="top-left"
-            />
             <Menu iconShape="round" >
                 <SubMenu title="Components" icon={<FaProductHunt />}>
-                <MenuItem>Produto 1</MenuItem>
-                <MenuItem>Produto 2</MenuItem>
+                {console.log(props.products.length)}
+                {props.products.length > 0 && props.products.forEach(product => {
+                    return(
+                        <MenuItem>{product.name}</MenuItem>
+                    )
+                })}
+                <MenuItem>Cadastrar Produto</MenuItem>
                 </SubMenu>
             </Menu>
         </ProSidebar>
