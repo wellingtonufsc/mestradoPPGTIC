86fe0373b2d33fc6b46d6da44bc1dd2c0351dfde
Hamilton-Dias <hamiltondias1997@hotmail.com>
2021-01-03 20:07:04 -0300

    new api

--- appiotafront/src/services/api.js
+++ appiotafront/src/services/api.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
 
-const api = Axios.create({ baseURL: 'http://localhost:3001/api' });
+const api = Axios.create({ baseURL: 'https://appiotaback.herokuapp.com/api' });
 
 export default api;
