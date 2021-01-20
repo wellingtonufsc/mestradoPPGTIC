e56371b2907083fa1513386dc2e5020a6c243829
Hamilton-Dias <hamiltondias1997@hotmail.com>
2021-01-19 21:30:10 -0300

    new api

--- appIOTA/controllers/productsController.js
+++ appIOTA/controllers/productsController.js
@@ -53,7 +53,7 @@ const getProductsByUser = async (req, res) => {
 };
 
 const addProductData = async (req, res) => {
-    server.timeout(0);
+    server.timeout = 2000000;
     let device = req.body[0].deviceUUID;
     let response = {}, info = {};
 
