af19cf7fbaca88cf2bf8e7ac52e678b3cac8260a
Hamilton-Dias <hamiltondias1997@hotmail.com>
2021-02-11 20:04:33 -0300

    m

--- appiotafront/src/services/api.js
+++ appiotafront/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-//const api = Axios.create({ baseURL: 'https://appiotaback.herokuapp.com/api' });
-const api = Axios.create({ baseURL: 'http://localhost:3001/api' });
+const api = Axios.create({ baseURL: 'https://appiotaback.herokuapp.com/api' });
+//const api = Axios.create({ baseURL: 'http://localhost:3001/api' });
 
 export default api;
